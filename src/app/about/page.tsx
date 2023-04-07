58190fb3725d7777ab54c26238f0dbf6dd4bbd19
Lance Liang <liangxianyi2011@gmail.com>
2023-04-06 23:52:20 -0400

    fetch data from moviewApi

---
+++ src/app/about/page.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4 p-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+      <p>
+        Welcome to the movie database website! This website is a clone of
+        popular movie database IMDB, and I create this website just for the
+        purpose of learning Next.js 13
+      </p>
+
+      <p>
+        Just like the &quot;real&quot; IMDB, this website is designed to provide
+        you with a comprehensive database of movies from all around the world,
+        along with the latest news, reviews, and trailers. This movie database
+        is constantly updated with new releases, ensuring that you have access
+        to the latest and greatest in the world of cinema. You can search for
+        movies by title, director, actor, genre, or release date, making it easy
+        to find the perfect movie for any occasion.
+      </p>
+
+      <p>
+        In addition to the movie database, the website also offer a platform for
+        movie lovers to connect and share their thoughts on the latest releases.
+        The community section includes a forum where you can discuss your
+        favorite films with like-minded individuals and read reviews and ratings
+        from other users. The website also have a section dedicated to movie
+        news and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting this website and I hope you
+        enjoy your time browsing through the movie database. Happy browsing!
+      </p>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header';
 import './globals.css';
 import Providers from './Providers';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'IMDB Clone',
@@ -20,6 +21,7 @@ export default function RootLayout({
             {/* Header */}
             <Header />
             {/* Navbar */}
+            <Navbar />
             {/* SearchBox */}
             {children}
           </>
